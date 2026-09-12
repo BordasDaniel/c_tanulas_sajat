8088c324658d7c3d8cf35ff0cf542e9deeeca020
BordasDaniel <bordas.daniel0124@gmail.com>
2026-09-12 23:03:14 +0200

    madlibs_project

---
+++ madlibs_project/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main()
+{
+    char adjective[20];
+    char noun[50];
+    char adjective2[20];
+    char verb[20];
+    char adjective3[20];
+
+    
+    printf("Enter an adjective (description): ");
+    fgets(adjective, sizeof(adjective), stdin);
+    adjective[strlen(adjective) - 1] = '\0';
+
+    printf("Enter a noun (animal or person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) - 1] = '\0';
+
+    printf("Enter a verb (ending w/ -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) - 1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) - 1] = '\0';
+
+    printf("\nToday I went to a %s zoo.\n", adjective);
+    printf("In an exhibit, I saw a %s.\n", noun);
+    printf("%s was %s and %s!\n", noun, adjective2, verb);
+    printf("I was %s!\n", adjective3);
+
+    return 0;
+}
