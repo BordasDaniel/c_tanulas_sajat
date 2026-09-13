cb06ef48e322cc1a630c6371ed1d13fe8b76840d
BordasDaniel <bordas.daniel0124@gmail.com>
2026-09-13 10:21:55 +0200

    circle calc project

---
+++ circle_calc_project/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+void puffer_cleaner()
+{
+    int c;
+    while((c = getchar()) != '\n' && c != '\0');
+}
+
+int main()
+{
+    const double pi = M_PI;
+    double radius;
+    double area;
+    double surface_area;
+    double volume;
+
+    printf("Give me the radius of your circle in cm: ");
+    scanf("%lf", &radius);
+    puffer_cleaner();
+
+
+    area =  pi * pow(radius, 2);
+    surface_area =  4* pi * pow(radius, 2);
+    volume = (4.0/3) * pi * pow(radius, 3);
+
+    printf("Area: %.4lfcm\n", area);
+    printf("Surface Area: %.4lfcm^2\n", surface_area);
+    printf("Volume: %.4lfcm^3\n", volume);
+
+
+
+    return 0;
+}
